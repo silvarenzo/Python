834887f56e4cb45ce2075ff6891133f3502b4c5f
renzo <renzo.silva96@hotmail.com>
2023-07-03 22:03:16 -0300

    curso python

---
+++ variables/Variables.py
@@ -0,0 +1,51 @@
+#nombre = "renzo "
+#bienvenida = f"Hola {nombre} ¿como estas?"
+#print ("renzo" in bienvenida)
+
+#A=8
+#B=5
+#C=A+B
+
+#print(C)
+
+
+#CONCATENACION 
+
+#nombre="renzo"
+
+#bienvenido = "hola " + nombre + " Como estas?"#
+
+#print (bienvenido)
+
+#la foma optimizada es asi, 
+
+nombre = "5"
+bienvenida = f"Hola {nombre} ¿como estas?"
+print ( bienvenida)
+
+#estos son los f string, lo cual hace que un dato cualquiera me lo convierte a texto.
+
+#operar del es un operador para borrar datos,
+
+#Definiendo una variable 
+
+nombre= "Renzo "
+
+#concatenar con + 
+
+bienvenida = f"Hola {nombre} ¿Como estas? "
+
+#operadores de pertenencia (in / not in)
+
+print ("Lucas" in bienvenida) #true
+print ("Lucas" not in bienvenida) #false
+
+
+
+
+
+
+
+
+
+
